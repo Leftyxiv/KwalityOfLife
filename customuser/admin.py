99eb6d7414206f6e8f7ac7202a947c747eef6485
Leftyxiv <manny@aledoux.net>
2021-07-06 12:32:16 -0400

    custom user model is in

--- customuser/admin.py
+++ customuser/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+  add_form = UserCreationForm
+  form = UserChangeForm
+  model = CustomUser
+  list_display = ['username', 'email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- customuser/models.py
+++ customuser/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class CustomUser(AbstractUser):
+  pass
 
-# Create your models here.
+  def __str__(self):
+    return self.username
