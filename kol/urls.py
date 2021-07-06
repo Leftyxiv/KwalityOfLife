db45d23004dea57dd65a6b1e6e34a36b3c187d7d
Leftyxiv <manny@aledoux.net>
2021-07-06 13:17:53 -0400

    index page is rendering the posts properly for the time being

--- kol/urls.py
+++ kol/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from posts.views import PostHomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', PostHomeView),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,2 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
+def PostHomeView(request, *args, **kwargs):
+  posts = Post.objects.all()
+  return render(request, 'productindex.html', context={'posts': posts})
