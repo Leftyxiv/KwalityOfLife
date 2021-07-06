68d9ab004f50ef45991fb51a835775c54e00d712
Ron ORourke <rono.business@gmail.com>
2021-07-06 14:09:16 -0400

    Started on api.views and added urls.py

--- kol/urls.py
+++ kol/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from api.urls import urlpatterns as api_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += api_urls
