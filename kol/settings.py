ae7636e56db1aec0a0dbab0dc6da0b33b05f8aea
Leftyxiv <manny@aledoux.net>
2021-07-06 13:12:02 -0400

    form is in was able to add a new post via the admin panel

--- kol/settings.py
+++ kol/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # group made apps
     'customuser',
+    'posts',
 ]
 
 MIDDLEWARE = [
--- posts/models.py
+++ posts/models.py
@@ -5,5 +5,9 @@ class Post(models.Model):
   user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
   created_at = models.DateTimeField(auto_now_add=True, blank=True)
   title = models.CharField(max_length=200)
-  product_image = models.ImageField(upload_to='uploads/', height_field="200px", width_field="200px", max_length=100)
+  company_website = models.URLField(blank=True, null=True)
+  product_image = models.ImageField(upload_to='uploads/', max_length=100)
   description = models.TextField()
+
+  def __str__(self):
+    return self.title
