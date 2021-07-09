ac17b2b6ffd4244568b24e59631ce760c585c807
Bexporter <adebexporter@gmail.com>
2021-07-09 11:07:13 -0400

    added the dot env and secret key

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,5 @@ class Post(models.Model):
   company_website = models.URLField(blank=True, null=True)
   product_image = models.ImageField(upload_to='uploads/', max_length=100)
   description = models.TextField()
-
   def __str__(self):
     return self.title
