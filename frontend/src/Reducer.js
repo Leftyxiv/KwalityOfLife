53bbc6c0a7c509d8c433d9246d063c8b5c79533c
Leftyxiv <manny@aledoux.net>
2021-07-09 17:53:59 -0400

    added a root reducer

---
+++ frontend/src/Reducer.js
@@ -0,0 +1,11 @@
+// create a root reducer
+import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+
+const createRootReducer = (history) => {
+  combineReducers({
+    router: connectRouter(history)
+  });
+};
+
+export default createRootReducer;
--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 
 const SignupForm = () => {
-  return (
-    <div>
-      
-    </div>
+    const [username, setUsername] = useState("");
+    const [pass, setPass] = useState("")
+  
+    const login = 'http://127.0.0.1:8000/api/token/login/'
+  
+    const onSub = async (e) => {
+      e.preventDefault()
+      const res = await axios.post(login, {
+        username,
+        password: pass
+      })
+      // console.log(res.data['auth_token'])
+      const cookies = new Cookies()
+      cookies.set('Bearer', res.data['auth_token'])
+      console.log(cookies.get('Bearer'))
+    }
+    return (
+      <div className='form-bg'>
+        <form onSubmit={onSub}>
+          <label style={{'color': 'black'}}>Username </label>
+        <input type='text' value={username} onChange={(e) => setUsername(e.target.value)}></input>
+        <br />
+          <label style={{'color': 'black'}}>Password </label>
+        <input type='password'  value={pass} onChange={(e) => setPass(e.target.value)}></input>
+        <br />
+      <input type='submit' />
+    </form>
+      </div>
   )
 }
 
