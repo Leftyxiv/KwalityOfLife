16ce5e10d5e4e396c01feab661f3f4003c85cc96
Leftyxiv <manny@aledoux.net>
2021-07-10 04:53:51 -0400

    added log in actions

--- frontend/src/Reducer.js
+++ frontend/src/Reducer.js
@@ -4,12 +4,14 @@ import { connectRouter } from 'connected-react-router';
 // import { createBrowserHistory } from 'history';
 
 import { signupReducer } from './components/SignupReducer';
+import { LoginReducer } from './components/LoginReducer';
 
 // const history = createBrowserHistory()
 const createRootReducer = (history) => {
   return combineReducers({
     router: connectRouter(history),
     createUser: signupReducer,
+    auth: LoginReducer,
   });
 };
 
---
+++ frontend/src/components/LoginActions.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import { push } from 'connected-react-router';
+import { toast } from 'react-toastify';
+import { LoginTypes } from './LoginTypes';
+import { setAxiosToken , toastError } from '../utils/Utils';
+
+export const login = (user, redirect) => dispatch => {
+  axios.post("/api/token/login", user)
+        .then(res => {
+          const { auth_token } = res.data;
+          setAxiosToken(auth_token);
+          dispatch(setToken(auth_token));
+          dispatch(getCurrentUser(redirect));
+        }).catch(err => {
+            dispatch(unsetCurrentUser());
+            toastError(err);
+        })
+}
+
+export const getCurrentUser = redirect => dispatch => {
+  axios.get('/api/users/me').then((res) => {
+    const user = {
+      username: res.data.username,
+    }
+    dispatch(setCurrentUser(user, redirect));
+  }).catch(err => {
+    dispatch(unsetCurrentUser(user, redirect));
+    toastError(err);
+  })
+}
+
+export const setToken = (token) => dispatch => {
+  setAxiosToken("");
+  localStorage.setItem('token', token);
+  dispatch({
+    type: LoginTypes.SET_TOKEN,
+    payload: token,
+  });
+};
+
+export const setCurrentUser = (user, redirect) => dispatch => {
+  localStorage.setItem('user', JSON.stringify(user));
+  dispatch({
+    type: LoginTypes.SET_USER,
+    payload: user,
+  });
+  if(redirect !== ""){
+    dispatch(push(redirect))
+  }
+}
+
+export const unsetCurrentUser = () => dispatch => {
+  setAxiosToken("");
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
+  dispatch({
+    type: LoginTypes.UNSET_USER
+  })
+}
+
+export const logout = () => dispatch => {
+  axios.post('/api/token/logout').then(res => {
+    dispatch(unsetCurrentUser());
+    dispatch(push("/"));
+    toast.success('Logged out!')
+  }).catch(err => {
+    dispatch(unsetCurrentUser());
+    toastError(err);
+  })
+}
