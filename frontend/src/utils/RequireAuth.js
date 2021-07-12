0621e9307ed61755cb5edffa389d80c3ace4414b
Leftyxiv <manny@aledoux.net>
2021-07-12 14:54:45 -0400

    added require auth function for anything not the landing page

--- frontend/src/utils/RequireAuth.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { push } from 'connected-react-router';
-import PropTypes from 'prop-types';
-
-export default requireAuth = ()
