46a18b8e5a4cb2a912fa188b777d89143c4642ac
Leftyxiv <manny@aledoux.net>
2021-07-12 14:54:45 -0400

    added require auth function for anything not the landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import { ToastContainer } from 'react-toastify';
-// import { Switch, Route } from 'react-router';
+import { Switch, Route } from 'react-router';
 
 import Root from './Root';
 
 import Navbar from './components/Navbar';
-// import Landing from './components/Landing';
+import Landing from './components/Landing';
 // import PostCard from './components/PostCard';
 // import PostList from './components/PostList';
 // import SignupForm from './components/SignupForm';
 // import LoginForm from './components/LoginForm';
 import Dashboard from './components/Dash';
+import requireAuth from './components/RequireAuth';
 import './App.css'
 
 function App() {
@@ -20,46 +21,8 @@ function App() {
         <ToastContainer hideProgressBar={true} newestOnTop={true} />
         <div className='bars'>
         <Navbar />
-        {/* <Switch>
-          <Route exact path="/">
-            <Landing />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/post/:id">
-            <PostDetail />
-          </Route>
-          <Route path="/author/:id">
-            <AuthorDetail />
-          </Route>
-          <Route path="/addpost">
-            <CreatePost />
-          </Route>
-          <Route path="/myaccount">
-            <ChangeUserDetail />
-          </Route>
-        </Switch> */}
-        
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-          {/* <Switch> */}
-        <Dashboard />
+        <Route exact path='/' component={Landing} />
+        <Route path='/*' component={requireAuth(Dashboard)} />
         {/* <Switch>
           <Route exact path="/" render={() => <Landing />} />
           <Route exact path="/feed" render={() => <PostList />} />
