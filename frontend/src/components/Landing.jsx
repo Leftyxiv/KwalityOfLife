1c32b5e3110353cac8b161e6ddb7bf4d07d4d67c
Ron ORourke <rono.business@gmail.com>
2021-07-14 13:37:10 -0400

    Pulled from origin

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -4,36 +4,36 @@ import './Landing.css';
 import LoginForm from './LoginForm';
 
 const Landing = () => {
-  let name = ['']
-  if(localStorage.getItem('user')){
-    const thisuser = localStorage.getItem('user');
-    const regex = /{"username":"(\w+)"}/
-    name = thisuser.match(regex)
-  }
+  // let name = ['','']
+  // if(localStorage.getItem('user')){
+  //   const thisuser = localStorage.getItem('user');
+  //   const regex = /{"username":"(\w+)"}/
+  //   name = thisuser.match(regex)
+  // }
   return (
-    <div>
-      {name != '' ? (
-        <header className='bg-image'>
-          <div className='bg-wrapper'>
-            <h1>Kwality of Life!</h1>
-            <h2>Where we share all the coolest buys and quality of life upgrades!</h2>
-            <LoginForm />
-          </div>
-        </header>
-      ) : (
-        <header>Home Page</header>
-      )}
-      
-    </div>
     // <div>
-    //   <header className='bg-image'>
+    //   {!name ? (
+    //     <header className='bg-image'>
     //       <div className='bg-wrapper'>
     //         <h1>Kwality of Life!</h1>
     //         <h2>Where we share all the coolest buys and quality of life upgrades!</h2>
     //         <LoginForm />
     //       </div>
-    //   </header>
+    //     </header>
+    //   ) : (
+    //     <h1>Home Page</h1>
+    //   )}
+      
     // </div>
+    <div>
+      <header className='bg-image'>
+          <div className='bg-wrapper'>
+            <h1>Kwality of Life!</h1>
+            <h2>Where we share all the coolest buys and quality of life upgrades!</h2>
+            <LoginForm />
+          </div>
+      </header>
+    </div>
   )
 }
 
