5590a19002b34e47532cd73d6f9574fce90a2ab4
Leftyxiv <manny@aledoux.net>
2021-07-16 08:36:35 -0400

    updated the landing page for more reusability

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,10 @@ import Root from './Root';
 
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
-// import PostCard from './components/PostCard';
-// import PostList from './components/PostList';
-// import SignupForm from './components/SignupForm';
-// import LoginForm from './components/LoginForm';
 import Dashboard from './components/Dash';
 import requireAuth from './components/RequireAuth';
 import SignupForm from './components/SignupForm';
+import LoginForm from './components/LoginForm';
 import './App.css'
 
 function App() {
@@ -24,8 +21,8 @@ function App() {
         <div className='bars'>
         <Navbar />
         <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route exact path="/signup" render={() => <SignupForm /> } />
+        <Route exact path='/' render={() => <Landing Component={LoginForm}/>} />
+        <Route exact path="/signup" render={() => <Landing Component={SignupForm} /> } />
         <Route exact path='/*' component={requireAuth(Dashboard)} />
         </Switch>
         </div>
